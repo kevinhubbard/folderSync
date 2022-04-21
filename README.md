878f8b6038878ac14a51907709d27d7819ae95a6
Kevin H <dgkallday98@gmail.com>
2022-04-21 16:51:19 -0400

    updates

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
 # folderSync
-node.js app which synchronizes files in 2 folders.
-
-## To run app
-* MAKE SURE YOU HAVE NODE.JS VERSION 10.15.1 INSTALLED
-* git clone repo in terminal
-* cd into folderSync directory
-* run command 'node folderSync.js (../path/to/inputFolder/) (../path/to/outputFolder/)'
-
-### Example 
-node folderSync.js ./folderA/ ./folderB/
-
-the app takes 2 command line arguements which are the paths to the input and output folders respectively. 
+Java app which synchronizes files in 2 folders.
---
+++ source/FolderSync.java
@@ -0,0 +1,85 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+import javax.swing.SpringLayout;
+
+public class FolderSync {
+	JLabel iLabel = new JLabel("");
+	JLabel fLabel = new JLabel("");
+
+	public void createGui() {
+		JFrame frame = new JFrame("Folder Sync");
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		JPanel topPane = new JPanel();
+		topPane.setBackground(Color.RED);
+		JPanel midPane = new JPanel();
+		midPane.setLayout(new BoxLayout(midPane, BoxLayout.PAGE_AXIS));
+		
+		JPanel botPane = new JPanel();
+		botPane.setBackground(Color.BLUE);
+		JButton iBtn = new JButton("Initial Folder");
+		iBtn.addActionListener(new InitialListener());
+		
+		JButton fBtn = new JButton("Synced Folder");
+		fBtn.addActionListener(new SyncListener());
+		
+		JLabel infoLabel = new JLabel("Choose an initial folder to syncronize with another.");
+		JButton syncBtn = new JButton("Sync");
+		JButton resetBtn = new JButton("Reset");
+
+		JPanel ipane = new JPanel();
+		JPanel fpane = new JPanel();
+		ipane.setBackground(Color.GREEN);
+		fpane.setBackground(Color.PINK);
+		topPane.add(infoLabel);
+		ipane.add(iBtn);
+		ipane.add(iLabel);
+		fpane.add(fBtn);
+		fpane.add(fLabel);
+		midPane.add(ipane);
+		midPane.add(fpane);
+		botPane.add(resetBtn);
+		botPane.add(syncBtn);
+		panel.add(topPane);
+		panel.add(midPane);
+		panel.add(botPane);
+		frame.add(panel);
+		frame.setSize(new Dimension(750,500));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	private void selectedFolder(String o) {
+		JFileChooser jfc = new JFileChooser();
+		//jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int returnVal = jfc.showOpenDialog(null);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			String pathloc = jfc.getSelectedFile().getAbsolutePath();
+			if (o.equals("i")) {
+				iLabel.setText(pathloc);
+			} else if (o.equals("f")) {
+				fLabel.setText(pathloc);
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		FolderSync fs = new FolderSync();
+		fs.createGui();
+	}
+
+	private class InitialListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			selectedFolder("i");
+		}
+	}
+
+	private class SyncListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			selectedFolder("f");
+		}
+	}
+}
